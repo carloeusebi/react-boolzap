ea54001be1e7a2f4ca1eccb596cfd88ba23780e6
carloeusebi <carloeusebi@gmail.com>
2023-06-27 14:30:42 +0200

    Hidden notifications

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Notification = () => {
+  const [state, setState] = useState(true);
+
   return (
     <div id="notifications">
-      <div className="contact">
-        <div className="avatar d-flex justify-center align-center">
-          <i className="fa-solid fa-bell-slash fa-xl"></i>
+      {state && (
+        <div className="contact">
+          <div className="avatar d-flex justify-center align-center">
+            <i className="fa-solid fa-bell-slash fa-xl"></i>
+          </div>
+          <div>
+            <p className="name">Receive new message notifications</p>
+            <p
+              onClick={() => setState(false)}
+              className="activate-notification-btn"
+            >
+              Activate desktop notifications
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="name">Receive new message notifications</p>
-          <p className="activate-notification-btn">
-            Activate desktop notifications
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
